02742ff7302a0cd9e22e599463fd3690a2e6c907
durvesh29607-jpg <durvesh29607@gmail.com>
2026-06-16 20:44:22 +0530

    Initial commit

---
+++ Arrays.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+#creating a 1D array
+x = np.array([1,2,3])
+
+#creating a 2D array
+y = np.array([[1,2],[3,4]])
+
+#creating a 3D array
+z =  np.array([[[1,2], [3,4]] , [[5,6], [7,8]]])
+
+print(x)
+print(y)
+print(z)
+
